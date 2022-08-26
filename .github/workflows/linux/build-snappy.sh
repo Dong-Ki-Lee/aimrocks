d91fa06edf6a6f274ef8809865910e399a5ddbaa
Dongki Lee <waitlee@pubg.com>
2022-08-26 16:49:29 +0900

    upload for linux

--- .github/workflows/linux/build-snappy.sh
+++ .github/workflows/linux/build-snappy.sh
@@ -3,6 +3,7 @@ set -e
 
 export AIM_DEP_DIR=/opt/aimrocks_deps
 export LD_LIBRARY_PATH=$AIM_DEP_DIR/lib:$LD_LIBRARY_PATH
+export CMAKE=/home/ec2-user/cmake-3.24.1-linux-aarch64/bin/cmake
 
 cd $AIM_DEP_DIR
 curl -L https://github.com/google/snappy/archive/1.1.8.tar.gz -o snappy-1.1.8.tar.gz
